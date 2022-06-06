e64aab79f4ae2741067f5de45869a5dc6c896607
uroeMalam <rizqullahsahputra153@gmail.com>
2022-06-06 10:48:16 +0700

    optimasi soal quiz nomor 1

--- Week01/Day05/final-quiz/number1.js
+++ Week01/Day05/final-quiz/number1.js
@@ -3,12 +3,23 @@
 // Buat script program menggunakan looping for untuk menampilkan deret diatas
 
 
+// cara petama ku
+// function deretAngka(banyakAngka, KelipatanBerapa){
+//     var deret = ''
+//     for(var i = 1; i <= banyakAngka; i++){
+//         if (i % KelipatanBerapa == 0){
+//             deret += i + ' ';
+//         }             
+//     }
+//     return deret;
+// }
+
+
+// cara yang lebih efisien setelah melihat punya teman teman yang lain
 function deretAngka(banyakAngka, KelipatanBerapa){
     var deret = ''
-    for(var i = 1; i <= banyakAngka; i++){
-        if (i % KelipatanBerapa == 0){
+    for(var i = KelipatanBerapa; i <= banyakAngka; i+=KelipatanBerapa){
             deret += i + ' ';
-        }             
     }
     return deret;
 }
